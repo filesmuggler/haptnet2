b438ff3a85e1e7c12e3aecbf813fa25a958be876
filesmuggler <krzste09@outlook.com>
2022-01-27 15:59:27 +0100

    removed deprecated function

--- loaders/HAPTNET_loader/HaptnetLoader.py
+++ loaders/HAPTNET_loader/HaptnetLoader.py
@@ -17,32 +17,6 @@ class HAPTNET_Loader:
         self.classes = self.dataset['classes']
         infile.close()
 
-    def _filter_dataset(self):
-        extracted_signals_train = np.array([self.dataset['train_ds'][i]['signal'][:, self.modalities] for i in range(len(self.dataset['train_ds']))])
-        extracted_labels_train = [self.dataset['train_ds'][i]['label'] for i in range(len(self.dataset['train_ds']))]
-
-        extracted_signals_val = np.array([self.dataset['val_ds'][i]['signal'][:, self.modalities] for i in range(len(self.dataset['val_ds']))])
-        extracted_labels_val = [self.dataset['val_ds'][i]['label'] for i in range(len(self.dataset['val_ds']))]
-
-        extracted_signals_test = np.array([self.dataset['test_ds'][i]['signal'][:, self.modalities] for i in range(len(self.dataset['test_ds']))])
-        extracted_labels_test = [self.dataset['test_ds'][i]['label'] for i in range(len(self.dataset['test_ds']))]
-
-        extracted_signals_norm_train = [(extracted_signals_train[i] - np.mean(extracted_signals_train[i])) / np.std(extracted_signals_train[i]) for i in range(len(extracted_signals_train))]
-        extracted_signals_norm_val = [(extracted_signals_val[i] - np.mean(extracted_signals_val[i])) / np.std(extracted_signals_val[i]) for i in range(len(extracted_signals_val))]
-        extracted_signals_norm_test = [(extracted_signals_test[i] - np.mean(extracted_signals_test[i])) / np.std(extracted_signals_test[i]) for i in range(len(extracted_signals_test))]
-
-        self.extracted_dataset_train = [(extracted_labels_train[i], extracted_signals_train[i]) for i in range(len(extracted_signals_train))]
-        self.extracted_dataset_val = [(extracted_labels_val[i], extracted_signals_val[i]) for i in range(len(extracted_signals_val))]
-        self.extracted_dataset_test = [(extracted_labels_test[i], extracted_signals_test[i]) for i in range(len(extracted_signals_test))]
-
-        self.extracted_dataset_norm_train = [(extracted_labels_train[i], extracted_signals_norm_train[i]) for i in
-                                        range(len(extracted_signals_norm_train))]
-        self.extracted_dataset_norm_val = [(extracted_labels_val[i], extracted_signals_norm_val[i]) for i in
-                                      range(len(extracted_signals_norm_val))]
-        self.extracted_dataset_norm_test = [(extracted_labels_test[i], extracted_signals_norm_test[i]) for i in
-                                       range(len(extracted_signals_norm_test))]
-
-
     def _pick_modalities(self):
         filtered_train = []
         filtered_test = []

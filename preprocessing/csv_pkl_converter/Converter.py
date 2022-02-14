03500250fc61c7e32e967a668c0b20c81da0af62
filesmuggler <krzste09@outlook.com>
2022-02-14 19:15:33 +0100

    Converter.py

---
+++ preprocessing/csv_pkl_converter/Converter.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+from scipy import signal
+
+class Converter:
+    def __init__(self):
+        print("hello")
+
+    def convert_dataset(self, path_to_dataset: str) -> None:
+        pass
+
+    def _dataset_to_pickle(self,path_to_data: str) -> None:
+        print("hello x2")
